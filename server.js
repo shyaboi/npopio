4b396e07bdf8bf87974ae28461992d1365629235
Shyaboi <ian.shane.sears@gmail.com>
2020-09-02 13:26:49 -0700

    fixing download link for new npop

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const PoP = require("npop");
 const n = new PoP()
+const zipGet = (req, res) =>{
+    const file = `${__dirname}/nPoP-installer-win32-x64.zip`;
+    res.download(file);
+}
   
-n.pop()
+n.pop(
+    undefined, 
+    zipGet, 
+    undefined, 
+    undefined,
+    [undefined,undefined, route3="/downloadzip"]
+    )
 
-
-  
